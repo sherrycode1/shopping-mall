189c831abd1e39bfdd5e37f69637949f3469b1b7
sherrycode1 <2509127733@qq.com>
2022-03-26 15:32:49 +0800

    m

--- src/main.js
+++ src/main.js
@@ -3,12 +3,15 @@ import App from './App.vue'
 import router from './router/index'
 import store from './store/index'
 import toast from 'components/common/toast'
-
+import VueLazyLoad from 'vue-lazyload'
 Vue.config.productionTip = false
 
 Vue.prototype.$bus = new Vue()
 // install toast 
 Vue.use(toast)
+
+// 使用懒加载插件
+Vue.use(VueLazyLoad)
 
 new Vue({
   router,
